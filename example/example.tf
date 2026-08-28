6e951030c818ee3dc98e4e5cf979cf82be30b40e
kunal-sharma-2803 <kunal2803sharma@gmail.com>
2026-08-28 10:47:11 +0530

    example file

---
+++ example/example.tf
@@ -0,0 +1,22 @@
+# get docs from https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+# here "example" is the internal referance of terraform not the name of VPC created
+
