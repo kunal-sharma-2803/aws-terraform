c2cf011669188a00075af1c61bdc18a8e64d5095
kunal-sharma-2803 <kunal2803sharma@gmail.com>
2026-09-15 10:45:18 +0530

    creating VPC peering connection

--- VPC peering/main.tf
+++ VPC peering/main.tf
@@ -1,11 +1,11 @@
 # for primary
 # creating primary VPC
 resource "aws_vpc" "primary_vpc" {
-  cidr_block       = var.primary_vpc_cidr
-  provider = aws.primary-region
+  cidr_block = var.primary_vpc_cidr
+  provider   = aws.primary-region
 
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 
   tags = {
     Name = "primary-vpc-${var.primary_region}"
@@ -14,7 +14,7 @@ resource "aws_vpc" "primary_vpc" {
 
 # subnet for primary VPC
 resource "aws_subnet" "primary_vpc_subnet" {
-  provider = aws.primary-region
+  provider   = aws.primary-region
   vpc_id     = aws_vpc.primary_vpc.id
   cidr_block = var.primary_vpc_subnet_cidr
 
@@ -23,7 +23,7 @@ resource "aws_subnet" "primary_vpc_subnet" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "primary-vpc-subnet-${var.primary_region}"
+    Name        = "primary-vpc-subnet-${var.primary_region}"
     Environment = "Dev"
   }
 }
@@ -31,10 +31,10 @@ resource "aws_subnet" "primary_vpc_subnet" {
 # internet gateway for primary VPC
 resource "aws_internet_gateway" "primary_vpc_igw" {
   provider = aws.primary-region
-  vpc_id = aws_vpc.primary_vpc.id
+  vpc_id   = aws_vpc.primary_vpc.id
 
   tags = {
-    Name = "primary-vpc-igw-${var.primary_region}"
+    Name        = "primary-vpc-igw-${var.primary_region}"
     Environment = "Dev"
   }
 }
@@ -42,7 +42,7 @@ resource "aws_internet_gateway" "primary_vpc_igw" {
 # route table for primary VPC
 resource "aws_route_table" "primary_vpc_rt" {
   provider = aws.primary-region
-  vpc_id = aws_vpc.primary_vpc.id
+  vpc_id   = aws_vpc.primary_vpc.id
 
   route {
     # for this destination
@@ -53,16 +53,44 @@ resource "aws_route_table" "primary_vpc_rt" {
   }
 
   tags = {
-    Name = "primary-vpc-rt-${var.primary_region}"
+    Name        = "primary-vpc-rt-${var.primary_region}"
     Environment = "Dev"
   }
 }
 
 # associate with primary VPC subnet
 resource "aws_route_table_association" "primary_vpc_rt_association" {
-  provider = aws.primary-region
+  provider       = aws.primary-region
   subnet_id      = aws_subnet.primary_vpc_subnet.id
   route_table_id = aws_route_table.primary_vpc_rt.id
+}
+
+# creating VPC connection 
+resource "aws_vpc_peering_connection" "primary_to_secondary" {
+  provider    = aws.primary-region
+  vpc_id      = aws_vpc.primary_vpc.id
+  peer_vpc_id = aws_vpc.secondary_vpc.id
+  peer_region = var.secondary_region
+
+  auto_accept = false
+
+  tags = {
+    Name        = "primary-to-secondary-peering"
+    Environment = "Dev"
+    Side        = "Requester"
+    # this side is for the connection setup process only, dont think in terms of sender and reciever 
+  }
+
+}
+
+# add route to primary rt 
+resource "aws_route" "primary_to_secondary_vpc_rt" {
+  provider                  = aws.primary-region
+  route_table_id            = aws_route_table.primary_vpc_rt.id
+  destination_cidr_block    = var.secondary_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.primary_to_secondary.id
+
+  depends_on = [aws_vpc_peering_connection_accepter.secondary_to_primary]
 }
 
 
@@ -71,11 +99,11 @@ resource "aws_route_table_association" "primary_vpc_rt_association" {
 # for secondary
 # creating secondary VPC
 resource "aws_vpc" "secondary_vpc" {
-  cidr_block       = var.secondary_vpc_cidr
-  provider = aws.secondary-region
+  cidr_block = var.secondary_vpc_cidr
+  provider   = aws.secondary-region
 
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 
   tags = {
     Name = "secondary-vpc-${var.secondary_region}"
@@ -84,7 +112,7 @@ resource "aws_vpc" "secondary_vpc" {
 
 # subnet for secondary VPC
 resource "aws_subnet" "secondary_vpc_subnet" {
-  provider = aws.secondary-region
+  provider   = aws.secondary-region
   vpc_id     = aws_vpc.secondary_vpc.id
   cidr_block = var.secondary_vpc_subnet_cidr
 
@@ -93,7 +121,7 @@ resource "aws_subnet" "secondary_vpc_subnet" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "secondary-vpc-subnet-${var.secondary_region}"
+    Name        = "secondary-vpc-subnet-${var.secondary_region}"
     Environment = "Dev"
   }
 }
@@ -101,10 +129,10 @@ resource "aws_subnet" "secondary_vpc_subnet" {
 # internet gateway for secondary VPC
 resource "aws_internet_gateway" "secondary_vpc_igw" {
   provider = aws.secondary-region
-  vpc_id = aws_vpc.secondary_vpc.id
+  vpc_id   = aws_vpc.secondary_vpc.id
 
   tags = {
-    Name = "secondary-vpc-igw-${var.secondary_region}"
+    Name        = "secondary-vpc-igw-${var.secondary_region}"
     Environment = "Dev"
   }
 }
@@ -112,7 +140,7 @@ resource "aws_internet_gateway" "secondary_vpc_igw" {
 # route table for secondary VPC
 resource "aws_route_table" "secondary_vpc_rt" {
   provider = aws.secondary-region
-  vpc_id = aws_vpc.secondary_vpc.id
+  vpc_id   = aws_vpc.secondary_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -120,14 +148,39 @@ resource "aws_route_table" "secondary_vpc_rt" {
   }
 
   tags = {
-    Name = "secondary-vpc-rt-${var.secondary_region}"
+    Name        = "secondary-vpc-rt-${var.secondary_region}"
     Environment = "Dev"
   }
 }
 
 # associate with secondary VPC subnet
 resource "aws_route_table_association" "secondary_vpc_rt_association" {
-  provider = aws.secondary-region
+  provider       = aws.secondary-region
   subnet_id      = aws_subnet.secondary_vpc_subnet.id
   route_table_id = aws_route_table.secondary_vpc_rt.id
 }
+
+# creating VPC connection 
+resource "aws_vpc_peering_connection_accepter" "secondary_to_primary" {
+  provider                  = aws.secondary-region
+  vpc_peering_connection_id = aws_vpc_peering_connection.primary_to_secondary.id
+  auto_accept               = true
+
+  tags = {
+    Name        = "secondary-to-primary-peering"
+    Environment = "Dev"
+    Side        = "Accepter"
+  }
+}
+
+# add route to secondary rt 
+resource "aws_route" "secondary_to_primary_vpc_rt" {
+  provider                  = aws.secondary-region
+  route_table_id            = aws_route_table.secondary_vpc_rt.id
+  destination_cidr_block    = var.primary_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.primary_to_secondary.id
+
+  depends_on = [aws_vpc_peering_connection_accepter.secondary_to_primary]
+}
+
+
